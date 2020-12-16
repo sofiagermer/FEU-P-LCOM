eaf401afa8af860a668c918c47578678d23b9eac
Sofia Ariana Moutinho Coimbra Germer <up201907461@fe.up.pt>
2020-12-16 00:43:20 +0000

    Update mouse.h

--- proj/src/mouse.h
+++ proj/src/mouse.h
@@ -6,21 +6,21 @@ int mouse_subscribe_int(uint16_t *bit_no);
 
 int mouse_unsubscribe_int();
 
-void read_status_register(uint8_t *stat);
+void mouse_read_status_register(uint8_t *stat);
 
 int mouse_check_status_register();
 
-int output_full();
+int mouse_output_full();
 
-int input_empty();
+int mouse_input_empty();
 
-int read_out_buffer(uint8_t *info);
+int mouse_read_out_buffer(uint8_t *info);
 
 void (mouse_ih)(void);
 
 void mouse_parse_packet(uint8_t packet[], struct packet *new_packet);
 
-int issue_command_to_kbc(uint8_t command, uint8_t arguments);
+int mouse_issue_command_to_kbc(uint8_t command, uint8_t arguments);
 
 int issue_command_to_mouse(uint8_t command);
 
@@ -28,7 +28,7 @@ uint8_t mouse_read_response();
 
 
 
-int issue_cmd_to_kbc(uint8_t command, uint8_t argument);
-int kbc_write_cmd(uint8_t command);
-int kbc_write_argument(uint8_t argument);
+int mouse_issue_cmd_to_kbc(uint8_t command, uint8_t argument);
+int mouse_kbc_write_cmd(uint8_t command);
+int mouse_kbc_write_argument(uint8_t argument);
 int mouse_write_command(uint8_t command, uint8_t*response);
